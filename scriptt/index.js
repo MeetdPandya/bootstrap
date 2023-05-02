0e6a170b754af8a3242bccfcc3a7a3320e09f250
Meet Pandya <meetd4541@gmail.com>
2023-05-02 21:46:10 +0530

    changes in basic layoutt

---
+++ scriptt/index.js
@@ -0,0 +1,20 @@
+var images = {};
+
+images = {
+    politics : [
+        {
+            "img_title":"img_1",
+            "img_desc":"img_desc1",
+            "img_src":"/img_1.jpg"
+        },
+        {
+
+        }
+    ]
+}
+
+function func1(){
+    images.politics.forEach(x => {
+        
+    });
+}
